bb8485797795693514b48aa919d4585a6fd0bc6e
LBT76240 <trailin_88_@hotmail.fr>
2017-11-18 22:42:01 +0100

    love merge

--- Assets/Scripts/InLove.cs
+++ Assets/Scripts/InLove.cs
@@ -7,35 +7,21 @@ public class InLove : MonoBehaviour {
     [SerializeField]
     GameObject poundingHeart;
 
-    [SerializeField]
-    [Tooltip("Heart beating when zombi is in love")]
-    AudioClip heartPounding;
-
-
-
-    private AudioSource audioSource;
-
     float heartLifeTime;
 
 
     // Use this for initialization
     void Start () {
-
-        heartLifeTime = 2f;
-        StartCoroutine(Wait(1f));
-        audioSource = GetComponent<AudioSource>();
+        if (GameObject.FindGameObjectWithTag("gamemanager").GetComponent<GameManager>().getCurrentScene() == 2) {
+            heartLifeTime = 2f;
+            StartCoroutine(Wait(1f));
+        }
     
     }
 
     // Update is called once per frame
     void Update () {
 
-        heartLifeTime -= Time.deltaTime;
-
-        if (heartLifeTime <= 0)
-        {
-            audioSource.Stop();
-        }
     }
 
     IEnumerator Wait(float waitTime)
@@ -47,13 +33,8 @@ public class InLove : MonoBehaviour {
 
     void StartPounding()
     {
-
         heartLifeTime = 2f;
         Destroy(Instantiate(poundingHeart, new Vector2(transform.position.x - 0.1f, transform.position.y + 2.2f), Quaternion.identity),heartLifeTime);
-        audioSource.clip = heartPounding;
-        audioSource.loop = false;
-        audioSource.Play();
-
-       
+      
     }
 }
