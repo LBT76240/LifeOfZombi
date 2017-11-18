9ed0fd02f748c39a8cbb60abd64028c7be6b976e
Kyoshiro10500 <victorperson10@hotmail.fr>
2017-11-19 00:16:20 +0100

    Début d'intéraction mémorisé avec le chat

--- Assets/Scripts/PNJ.cs
+++ Assets/Scripts/PNJ.cs
@@ -10,23 +10,46 @@ public abstract class PNJ : Interactible {
     [SerializeField]
     [Tooltip("PNJ's name")]
     string namePNJ;
+    public PNJ_State state_humain;
+    public PNJ_State state_zombie;
 
     /// <summary>
     ///     
     /// </summary>
+    /// 
 
     protected List<Action> listOfAction;
     protected int index = -1;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public int isAlreadyStated()
+    {
+        int i;
+        for (i = 0; i < GameObject.FindGameObjectWithTag("gamemanager").GetComponent<GameManager>().state_pnj.Count; i++)
+        {
+            if (GameObject.FindGameObjectWithTag("gamemanager").GetComponent<GameManager>().state_pnj[i] ==
+                state_zombie)
+            {
+                break;
+            }
+
+            if (GameObject.FindGameObjectWithTag("gamemanager").GetComponent<GameManager>().state_pnj[i] ==
+                state_humain)
+            {
+                break;
+            }
+        }
+        return i;
+    }
 
     abstract public void Interact(Action action);
 
