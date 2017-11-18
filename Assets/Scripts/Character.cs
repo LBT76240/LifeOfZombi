bf1e81da4702a9d1751e28101cdfa3fd631107ab
mehdi <mehdi.khadir@catenate.com>
2017-11-18 17:35:15 +0100

    Added a sound system for the character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,10 +11,27 @@ public class Character : Interactible {
     [Tooltip("Sets the movement speed of the zombie")]
     float speed = 1.5f;
 
-  
-
     Animator Animator;
 
+    [SerializeField]
+    [Tooltip("Sound played when clicking on the zombie")]
+    AudioClip rhhhSound;
+
+    [SerializeField]
+    [Tooltip("Sound played when walking")]
+    AudioClip walkSound;
+
+    private AudioSource audioSource;
+
+    /// <summary>
+    ///     True if the target toward the character must walk to is over the current position of the character when pointing and clicking
+    /// </summary>
+    private bool isTargetOverWhenClicking;
+
+    /// <summary>
+    ///     The character rotates to look at the mouse
+    /// </summary>
+    
     void FaceClickedPoint() {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
@@ -40,30 +57,47 @@ public class Character : Interactible {
         Animator = GetComponent<Animator>();
         Animator.StopPlayback();
         Animator.enabled = false;
+        audioSource = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0)) {
             UpdateTarget();
-            FaceClickedPoint();
- 
             Animator.enabled = true;
+            isTargetOverWhenClicking = GetComponent<BoxCollider2D>().bounds.Contains(target);
+
+            //  Play the sound and face the target only if the mouse has not clicked on the character
+            if (!isTargetOverWhenClicking)
+            {
+                FaceClickedPoint();
+                audioSource.clip = walkSound;
+                audioSource.loop = true;
+                audioSource.Play();
+            }
         }
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        if(transform.position==target)
+
+        if (transform.position == target)
         {
             Animator.Play(Animator.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, 0);
-            
+
+        }
+
+        //  Move only if the target is not on the character
+        if (!isTargetOverWhenClicking)
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        //  If the character reaches the target, stop playing the walking sound
+        if (Vector3.Distance(transform.position, target) < 0.1) {
+            audioSource.loop = false;
+            audioSource.Stop();
         }
     }
 
 
     override protected void OnMouseDownAction() {
-        //AudioSource audio = GetComponent<AudioSource>();
-        //audio.Play();
-        //audio.Play(44100);
+        audioSource.clip = rhhhSound;
+        audioSource.Play();
     }
 
     protected override void OnMouseRightAction() {
