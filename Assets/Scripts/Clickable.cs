e521192732af041f9018898978074996d9157510
LBT76240 <trailin_88_@hotmail.fr>
2017-11-18 11:02:55 +0100

    Ajout enum Action, list d'action, changement d'action click droit

--- Assets/Scripts/Clickable.cs
+++ Assets/Scripts/Clickable.cs
@@ -2,28 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Clickable : MonoBehaviour {
+public enum Action {
+    Prendre, Manger
+};
 
-    public Sprite mouseSprite;
+public abstract class Clickable : MonoBehaviour {
+    
+
+    //A mettre dans le GameManager
+    public Texture2D defaultCursor;
+    public Texture2D collectibleCursor;
+    public Texture2D mangerCursor;
+
+    public CursorMode curMod = CursorMode.Auto;
+    public Vector2 hotspot = Vector2.zero;
+
+    public Action action;
+    
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Cursor.SetCursor(defaultCursor, hotspot, curMod);
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    private void OnMouseOver() {
-        //TODO
-    }
+   
 
     protected abstract  void OnMouseDownAction();
 
     public void OnMouseDown() {
         OnMouseDownAction();
+    }
+
+    void OnMouseEnter() {
+        if (gameObject.tag == "Collectible") {
+            updateCursor();
+        }
+    }
+
+    public void updateCursor() {
+        if (action == Action.Prendre) {
+            Cursor.SetCursor(collectibleCursor, hotspot, curMod);
+        } else if (action == Action.Manger) {
+            Cursor.SetCursor(mangerCursor, hotspot, curMod);
+        }
+    }
+
+    protected abstract void OnMouseRightAction();
+
+    void OnMouseOver() {
+        if(Input.GetMouseButtonDown(1)){
+            OnMouseRightAction();
+        }
+    }
+
+
+    void OnMouseExit() {
+        Cursor.SetCursor(defaultCursor, hotspot, curMod);
     }
 
 }
--- Assets/Scripts/hover.cs
+++ Assets/Scripts/hover.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class hover : MonoBehaviour {
     public Texture2D defaultCursor;
     public Texture2D collectibleCursor;
+    public Texture2D mangerCursor;
 
     public CursorMode curMod = CursorMode.Auto;
     public Vector2 hotspot = Vector2.zero;
+
+    
 
     // Use this for initialization
     void Start () {
