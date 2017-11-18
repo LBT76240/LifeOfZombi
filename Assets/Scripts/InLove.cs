d6b971bd86a76dd83a0c36c761a894d2a2a2b35e
Samihael <32427349+Samihael@users.noreply.github.com>
2017-11-18 22:22:25 +0100

    quand un zombie est amoureux

---
+++ Assets/Scripts/InLove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InLove : MonoBehaviour {
+
+    [SerializeField]
+    GameObject poundingHeart;
+
+    [SerializeField]
+    [Tooltip("Heart beating when zombi is in love")]
+    AudioClip heartPounding;
+
+
+
+    private AudioSource audioSource;
+
+    float heartLifeTime;
+
+
+    // Use this for initialization
+    void Start () {
+
+        heartLifeTime = 2f;
+        StartCoroutine(Wait(1f));
+        audioSource = GetComponent<AudioSource>();
+    
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        heartLifeTime -= Time.deltaTime;
+
+        if (heartLifeTime <= 0)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    IEnumerator Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        StartPounding();
+       
+    }
+
+    void StartPounding()
+    {
+
+        heartLifeTime = 2f;
+        Destroy(Instantiate(poundingHeart, new Vector2(transform.position.x - 0.1f, transform.position.y + 2.2f), Quaternion.identity),heartLifeTime);
+        audioSource.clip = heartPounding;
+        audioSource.loop = false;
+        audioSource.Play();
+
+       
+    }
+}
