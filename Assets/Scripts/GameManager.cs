377fae4a1a6ea8632f9f73b6aa5a0dff679213df
Samihael <32427349+Samihael@users.noreply.github.com>
2017-11-18 11:17:42 +0100

    game manager + level manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    int currentLevel;
+
+    List<Collectible> items;
+
+    [SerializeField]
+    int maxMoral;
+
+    [SerializeField]
+    int minMoral;
+
+    int moral;
+
+    [SerializeField]
+    int time;
+
+    List<bool> actions;
+
+
+    public int CurrentLevel
+    {
+        get
+        {
+            return this.currentLevel;
+        }
+        set
+        {
+            this.currentLevel = value;
+        }
+    }
+
+    public List<Collectible> Items
+    {
+        get
+        {
+            return this.items;
+        }
+        set
+        {
+            this.items = value;
+        }
+    }
+
+    public int MaxMoral
+    {
+        get
+        {
+            return this.maxMoral;
+        }
+    }
+
+    public int MinMoral
+    {
+        get
+        {
+            return this.minMoral;
+        }
+    }
+
+    public int Moral
+    {
+        get
+        {
+            return this.moral;
+        }
+        set
+        {
+            this.moral = value;
+        }
+    }
+
+    public int Time
+    {
+        get
+        {
+            return this.time;
+        }
+    }
+
+    public List<bool> Actions
+    {
+        get
+        {
+            return this.actions;
+        }
+        set
+        {
+            this.actions = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : Clickable {
+
+    [SerializeField]
+    int nextLevel;
+
+
+    public int NextLevel
+    {
+        get
+        {
+            return this.nextLevel;
+        }
+        set
+        {
+            this.nextLevel = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void OnMouseDownAction()
+    {
+        
+            SceneManager.LoadScene("Scene"+nextLevel);
+       
+
+    }
+}
