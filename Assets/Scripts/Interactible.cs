888c7b2eeab7232e505e2f83a6a40fb9e9f6b8a7
LBT76240 <trailin_88_@hotmail.fr>
2017-11-18 09:40:21 +0100

    Ajout de Interactible

---
+++ Assets/Scripts/Interactible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactible : Clickable {
+
+    public int indiceAction = -1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
