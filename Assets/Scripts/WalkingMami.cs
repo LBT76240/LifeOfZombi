463ebd464e69097a92c33ec6c15965c2fcbf3704
Samihael <32427349+Samihael@users.noreply.github.com>
2017-11-19 00:02:40 +0100

    WALKING MAMI <3

---
+++ Assets/Scripts/WalkingMami.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingMami : MonoBehaviour {
+
+    Animator Animator;
+
+
+    // Use this for initialization
+    void Start () {
+        Animator = GetComponent<Animator>();
+        
+        Animator.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
