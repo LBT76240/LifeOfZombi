162211b8731e802605b3d44840e9a330773a7105
Kyoshiro10500 <victorperson10@hotmail.fr>
2017-11-18 21:04:05 +0100

    Résolution du bug d'animation lors du déplacement.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -25,7 +25,7 @@ public class Character : Interactible {
     [SerializeField]
     [Tooltip("Sound played when walking")]
     AudioClip walkSound;
-
+ 
     private AudioSource audioSource;
 
     /// <summary>
@@ -61,6 +61,8 @@ public class Character : Interactible {
         if (target.x > maxX) {
             target.x = maxX;
         }
+
+        
     }
 
     // Use this for initialization
@@ -100,8 +102,9 @@ public class Character : Interactible {
         }
 
         //  Move only if the target is not on the character
-        if (!isTargetOverWhenClicking)
+        
             transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
 
         //  If the character reaches the target, stop playing the walking sound
         if (Mathf.Abs(transform.position.x - target.x) < 0.1) {
@@ -110,7 +113,6 @@ public class Character : Interactible {
                 audioSource.loop = false;
                 audioSource.Stop();
             }
-            
             Animator.Play(Animator.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, 0);
         }
     }
@@ -120,7 +122,7 @@ public class Character : Interactible {
         audioSource.clip = rhhhSound;
         audioSource.loop = false;
         audioSource.Play();
-        
+        target.x = this.transform.position.x;
     }
 
     protected override void OnMouseRightAction() {
