5fa29a897c10715b46fcd69ae0757d00f9d2b41c
mehdi <mehdi.khadir@catenate.com>
2017-11-18 12:48:22 +0100

    Première version de la structure des PNJs

---
+++ Assets/Scripts/PNJ.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNJ : Interactible {
+
+    /// <summary>
+    ///     PNJ's name
+    /// </summary>
+    [SerializeField]
+    [Tooltip("PNJ's name")]
+    string name;
+
+    /// <summary>
+    ///     
+    /// </summary>
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void OnMouseDownAction() {
+        return;
+    }
+
+    protected override void OnMouseRightAction() {
+        return;
+    }
+
+    private void OnMouseOver() {
+        return;
+    }
+}
