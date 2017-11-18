e9f16577419ca2721cbf53701eb82b838f20c76f
LBT76240 <trailin_88_@hotmail.fr>
2017-11-18 10:05:08 +0100

    Ajout du changement de curseur

---
+++ Assets/Scripts/hover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hover : MonoBehaviour {
+    public Texture2D defaultCursor;
+    public Texture2D collectibleCursor;
+
+    public CursorMode curMod = CursorMode.Auto;
+    public Vector2 hotspot = Vector2.zero;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.SetCursor(defaultCursor, hotspot, curMod);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter() {
+        if(gameObject.tag=="Collectible") {
+            Cursor.SetCursor(collectibleCursor, hotspot, curMod);
+        }
+    }
+
+    void OnMouseExit() {
+        Cursor.SetCursor(defaultCursor, hotspot, curMod);
+    }
+
+}
