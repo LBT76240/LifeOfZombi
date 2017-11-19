d87c96fa4612eea7d69c671376a265ec0f25a6f6
LBT76240 <trailin_88_@hotmail.fr>
2017-11-19 12:16:24 +0100

    start animation scene 1

---
+++ Assets/Scripts/StartAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnim : MonoBehaviour {
+
+    public GameObject zombi;
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		if(GameObject.FindGameObjectWithTag("gamemanager").GetComponent<GameManager>().getLastScene() != 0) {
+            Destroy(gameObject);
+        } else {
+            zombi.SetActive(false);
+            StartCoroutine(Wait(1.8f));
+        }
+	}
+    IEnumerator Wait(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        zombi.SetActive(true);
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
