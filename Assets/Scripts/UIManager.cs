f02cd50cd358bb5fffd07d5aca7b310d59a64b71
Kyoshiro10500 <victorperson10@hotmail.fr>
2017-11-19 10:12:03 +0100

    SliderTime ready

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,21 +7,26 @@ public class UIManager : MonoBehaviour {
 
     [SerializeField]
     Slider slider;
+
+    [SerializeField]
+    Slider sliderTime;
+
     GameObject gameManager;
     public Sprite sprite_default;
     public Texture2D texture_default;
 	// Use this for initialization
 	void Start () {
        gameManager = GameObject.FindGameObjectWithTag("gamemanager");
+        slider.minValue = gameManager.GetComponent<GameManager>().MinMoral;
+        slider.maxValue = gameManager.GetComponent<GameManager>().MaxMoral;
+        sliderTime.maxValue = gameManager.GetComponent<GameManager>().Time;
     }
 	
 	// Update is called once per frame
 	void Update () {
         slider.value = gameManager.GetComponent<GameManager>().Moral;
-        slider.minValue = gameManager.GetComponent<GameManager>().MinMoral;
-        slider.maxValue = gameManager.GetComponent<GameManager>().MaxMoral;
-
-        if(slider.value < (slider.minValue + slider.maxValue)/2)
+        sliderTime.value = gameManager.GetComponent<GameManager>().currentTime;
+        if (slider.value < (slider.minValue + slider.maxValue)/2)
         { 
             GameObject.Find("SliderBack").GetComponent<Image>().color= new Color(99f/255f, 124f/255f, 99f/255f);
             GameObject.Find("SliderFill").GetComponent<Image>().color = new Color(1, 1, 1);
