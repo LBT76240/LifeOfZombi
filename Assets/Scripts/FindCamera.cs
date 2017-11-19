e8ef6416941297f9a7f8cc494d192e7c7774c410
LBT76240 <trailin_88_@hotmail.fr>
2017-11-19 09:41:12 +0100

    WHOA

---
+++ Assets/Scripts/FindCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class FindCamera : MonoBehaviour {
+
+    public VideoPlayer videoPlayer; 
+
+	// Use this for initialization
+	void Start () {
+        videoPlayer.targetCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(videoPlayer.frame== (long)videoPlayer.frameCount) {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/WhoaScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhoaScript : MonoBehaviour {
+
+    bool alreadyInterract = false;
+
+    public GameObject whoaprefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown() {
+        if(!alreadyInterract) {
+            print("whoa");
+            Instantiate(whoaprefab, new Vector3(0, 0, 0), Quaternion.identity);
+            alreadyInterract = true;
+        }
+    }
+}
