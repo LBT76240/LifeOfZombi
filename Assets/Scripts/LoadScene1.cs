3de1a5a2d53ccc6b6df225320d61e69f70a9045c
LBT76240 <trailin_88_@hotmail.fr>
2017-11-19 14:20:38 +0100

    version 1.4

--- Assets/Scripts/LoadScene1.cs
+++ Assets/Scripts/LoadScene1.cs
@@ -7,13 +7,16 @@ public class LoadScene1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(5f);
         GameObject.FindGameObjectWithTag("gamemanager").GetComponent<GameManager>().playMusicGraveYard();
         GameObject.FindGameObjectWithTag("gamemanager").GetComponent<GameManager>().changeCurrentScene(1);
         SceneManager.LoadScene("Scene" + 1);
     }
-	
-	// Update is called once per frame
-	void Update () {
+    void Update () {
 		
 	}
 }
